caf8b1269d688b2ff174db132cb748b2f9711721
front_byte <shifaskottayi@gmail.com>
2021-02-23 10:51:50 +0530

    message

---
+++ script.js
@@ -0,0 +1,9 @@
+const name = document.getElementById("fname")
+const email = document.getElementById("email")
+const mobile = document.getElementById("mobile")
+const form = document.getElementById("subject")
+const errorElement = dociment.getElementById(' error')
+  var name_error = document.getElementById('name_error')
+ 
+
+ 
